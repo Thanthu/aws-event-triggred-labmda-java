cc85f607df789ba8aae0cf62a1330d2c5c95763d
Thanthu <tnair@orthofx.com>
2021-11-18 13:02:32 +0530

    Implement BillManagementLambda. Closes #6

--- patientcheckout/src/main/java/com/thanthu/aws/lambda/s3sns/BillManagementLambda.java
+++ patientcheckout/src/main/java/com/thanthu/aws/lambda/s3sns/BillManagementLambda.java
@@ -1,9 +1,24 @@
 package com.thanthu.aws.lambda.s3sns;
 
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thanthu.aws.lambda.s3sns.model.PatientCheckoutEvent;
+
 public class BillManagementLambda {
-	
-	public void handler() {
-		
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	public void handler(SNSEvent snsEvent) {
+		snsEvent.getRecords().forEach(record -> {
+			try {
+				PatientCheckoutEvent patientCheckoutEvent = objectMapper.readValue(record.getSNS().getMessage(),
+						PatientCheckoutEvent.class);
+				System.out.println(patientCheckoutEvent);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 }
